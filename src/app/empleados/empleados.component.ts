03de3dd478712a34d15efbf0634cccf47d1323b6
Karen Elpidio <Karen73@live.com.ar>
2023-01-26 10:54:40 -0300

    build:se inicia el proyecto

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+// selector
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./empleados.component.html",
+    styleUrls: ["./empleados.component.css"],
+})
+
+// etiqueta
+export class EmpleadosComponent {
+
+}
