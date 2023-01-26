9dbf8ca86b248d22ac7b16a2811812360f885a95
Karen Elpidio <Karen73@live.com.ar>
2023-01-26 14:44:29 -0300

    feat:se implementa event binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -11,12 +11,21 @@ export class EmpleadoComponent {
 nombre="Juan"
 apellido="Marrubash"
 edad= 19;
-
 habilitacionCuadro= false; 
 userRegistrado= false;
+textoDeRegistro="No hay nadie registrado";
 
 getRegistroUsuario(){
-  this.userRegistrado = true;
+  this.userRegistrado = false;
+}
+
+setUsuarioRegistrado(event:Event){
+
+  if((<HTMLInputElement>event.target).value=="si") {
+    this.textoDeRegistro= "El usuario se acaba de registrar";
+  }else {
+    this.textoDeRegistro= "No hay nadie registrado";
+}
 }
 
 constructor() {}
